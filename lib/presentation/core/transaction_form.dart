43aa9dab98682e983080b722befef08bb1724940
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-11-24 12:20:02 +0200

    soem change

--- lib/presentation/core/transaction_form.dart
+++ lib/presentation/core/transaction_form.dart
@@ -1,10 +1,22 @@
+import 'package:cost_accounting/application/core/transaction_notifier.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 
-class TransactionForm extends StatelessWidget {
+final transactionNotifier =
+    ChangeNotifierProvider((ref) => TransactionNotifier());
+
+class TransactionForm extends StatefulWidget {
   const TransactionForm({Key? key}) : super(key: key);
 
   @override
+  State<TransactionForm> createState() => _TransactionFormState();
+}
+
+class _TransactionFormState extends State<TransactionForm> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    // TODO: implement build
+    throw UnimplementedError();
   }
 }
